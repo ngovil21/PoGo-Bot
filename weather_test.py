b4dc737eabf27aef4bab6d6a996d4048619c55b2
ngovil21 <ngovil21@gmail.com>
2017-12-15 10:32:51 -0500

    implement webhooks in pgpool

--- weather_test.py
+++ weather_test.py
@@ -1,11 +1,16 @@
 import requests
 import json
 import time
+import os
 
 ACCUWEATHER_API = "fikrPxMEvARvDvYjZ04HsmJfuArj4Fv1"
+ACCUWEATHER_ICON = "https://developer.accuweather.com/sites/default/files/{}-s.png"
 LOCATION = "60607"
 LOCAL_KEY = "2626574"
 DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/391080178060754956/LxynWpgS1SiTwA2pqyM_kDVtYvpwJzGyRlL4AGoR3eYbBr2lOTQmyGiqmH1Rg0VUnRUE"
+
+os.environ['TZ'] = 'America/Chicago'
+time.tzset()
 
 weather = requests.get(url="http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/{}?apikey={}&details=False"
                        .format(LOCAL_KEY, ACCUWEATHER_API))
@@ -14,15 +19,23 @@ weather = requests.get(url="http://dataservice.accuweather.com/forecasts/v1/hour
 
 weather_json = json.loads(weather.content.decode())
 
-content = "Weather for {}\n".format(LOCATION)
-print(weather_json)
+embeds = []
 for hour in weather_json:
-    t = time.localtime(int(hour['EpochDateTime']))
-    content += "{}: {}\n".format(time.strftime("%H:%M", t), hour["IconPhrase"])
+    lt = time.localtime(int(hour['EpochDateTime']))
+    description = "{}: {}\n".format(time.strftime("%H:%M", lt), hour["IconPhrase"])
+    embed = {
+        "title": time.strftime("%H:%M", lt),
+        "thumbnail": ACCUWEATHER_ICON.format(hour['WeatherIcon']),
+        "description": hour['IconPhrase'],
+        "url": hour['Link']
+    }
+    embeds.append(embed)
+
 
 data = {
     "username": "Accuweather",
-    "content": content
+    "avatar_url": "https://pbs.twimg.com/profile_images/879422659620163584/wudfVGeL_400x400.jpg",
+    "embeds": embeds
 }
 
 requests.post(url=DISCORD_WEBHOOK, data=data)
